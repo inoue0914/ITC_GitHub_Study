57ce83c0d73d9f5330c05de534c2d3197a418b12
b9429 <b9429@oic.jp>
2022-06-08 12:58:12 +0900

    プロジェクトの作成
    プロジェクトの作成

---
+++ ITC_Github_Study/ITC_Github_Study/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITC_Github_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

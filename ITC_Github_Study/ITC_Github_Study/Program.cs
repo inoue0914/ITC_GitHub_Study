fd07cba90dd956246d80edc57873aba06c3475e6
b9429 <b9429@oic.jp>
2022-06-08 13:49:49 +0900

    雛形プログラムの作成

--- ITC_Github_Study/ITC_Github_Study/Program.cs
+++ ITC_Github_Study/ITC_Github_Study/Program.cs
@@ -6,7 +6,21 @@ namespace ITC_Github_Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, m;
+
+            n = numInput();
+            m = numInput();
+
+            n += m;
+            Console.WriteLine(n);
+        }
+
+        static int numInput()
+        {
+            int num;
+            Console.Write("数字を入力してください ");
+            num = int.Parse(Console.ReadLine());
+            return num;
         }
     }
 }
